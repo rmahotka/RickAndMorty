5200426e321137d91812ec2c540212b0a42b741b
mahotich <rmahotka@gmail.com>
2024-05-27 13:55:07 +0300

    add pagination, add filter

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,128 @@
 <template>
   <HeaderItem />
   <div class="container">
-    <CardComponent :characters="charactersResult" />
+    <form class="filter" @submit.prevent="filter(filterName, filterStatus)">
+      <input type="text" v-model="filterName" />
+      <select name="status" id="filter-status" v-model="filterStatus">
+        <option value=""></option>
+        <option value="alive">Alive</option>
+        <option value="dead">Dead</option>
+        <option value="unknown">Unknown</option>
+      </select>
+      <button>Применить</button>
+    </form>
+    <CardComponent :characters="characters" />
+    <div class="pagination-block">
+      <button @click="prevPage" class="pagination-btn" :disabled="isFirstPage"><</button>
+      <span class="pageNumber">{{ pageItem }}</span>
+      <button @click="nextPage" class="pagination-btn" :disabled="isLasttPage">></button>
+    </div>
   </div>
 </template>
 
 <script setup>
 import HeaderItem from './components/HeaderItem.vue'
 import CardComponent from './components/CardComponent.vue'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 
-const charactersResult = ref([])
-const charactersInfo = ref({})
+const filterStatus = ref('')
+const filterName = ref('')
+const characters = ref()
+const pageItem = ref(1)
 
-const getData = () => {
-  fetch('https://rickandmortyapi.com/api/character')
+const filter = (name, status, page) => {
+  fetch(`https://rickandmortyapi.com/api/character?name=${name}&status=${status}&page=${page}`)
     .then((responce) => responce.json())
-    .then((res) => {
-      charactersResult.value = res.results
-      charactersInfo.value = res.info
-    })
-    .catch((err) => {
-      console.error(err)
+    .then((result) => {
+      characters.value = result.results
     })
 }
 
-onMounted(getData)
+const nextPage = () => {
+  pageItem.value++
+  window.scrollTo(pageYOffset, 0)
+  filter(filterName.value, filterStatus.value, pageItem.value)
+}
+
+const prevPage = () => {
+  pageItem.value--
+  window.scrollTo(pageYOffset, 0)
+  filter(filterName.value, filterStatus.value, pageItem.value)
+}
+
+const isFirstPage = computed(() => {
+  return pageItem.value === 1
+})
+
+const isLasttPage = computed(() => {
+  return pageItem.value === 41
+})
+
+onMounted(() => {
+  filter(filterName.value, filterStatus.value)
+})
 </script>
 
 <style lang="scss" scoped>
 .container {
   padding: 0 30px;
   background-color: #272b33;
+}
+
+.filter {
+  display: flex;
+  gap: 8px;
+  padding: 20px 0;
+
+  input {
+    padding: 6px 8px;
+    border-radius: 6px;
+    border: 0;
+  }
+
+  button {
+    padding: 6px;
+    border-radius: 6px;
+    border: 1px solid #fff;
+    color: #fff;
+    background: inherit;
+    cursor: pointer;
+    font-size: 16px;
+    transition: all ease 0.3s;
+
+    &:hover {
+      background-color: #fff;
+      color: #0d0f11;
+    }
+  }
+}
+
+.pageNumber {
+  font-size: 25px;
+  color: #fff;
+}
+
+.pagination-block {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-bottom: 20px;
+  gap: 20px;
+}
+
+.pagination-btn {
+  padding: 15px;
+  border-radius: 10px;
+  border: 1px solid #fff;
+  color: #fff;
+  font-size: 20px;
+  background: inherit;
+  cursor: pointer;
+  transition: all ease 0.3s;
+
+  &:hover {
+    background: #fff;
+    color: #0d0f11;
+  }
 }
 </style>
--- src/components/CardComponent.vue
+++ src/components/CardComponent.vue
@@ -1,15 +1,5 @@
 <template>
   <section class="block-container">
-    <form class="filter" @submit.prevent="filter">
-      <input type="text" v-model="filterName" />
-      <select name="status" id="filter-status">
-        <option value=""></option>
-        <option value="alive">Alive</option>
-        <option value="dead">Dead</option>
-        <option value="unknown">Unknown</option>
-      </select>
-      <button>Применить</button>
-    </form>
     <div class="item-container">
       <div v-for="item in props.characters" :key="item.id" class="block-item">
         <CardItem :character="item" />
@@ -23,17 +13,14 @@ import CardItem from './CardItem.vue'
 
 const props = defineProps({
   characters: {
-    type: Object,
     required: true
   }
 })
-
-const filter = () => {}
 </script>
 
 <style lang="scss" scoped>
 .block-container {
-  padding: 80px 0;
+  padding: 20px 0 80px 0;
 }
 
 .item-container {
@@ -44,33 +31,5 @@ const filter = () => {}
 
 .block-item {
   grid-column: 6 span;
-}
-
-.filter {
-  display: flex;
-  gap: 8px;
-  margin-bottom: 20px;
-
-  input {
-    padding: 6px 8px;
-    border-radius: 6px;
-    border: 0;
-  }
-
-  button {
-    padding: 6px;
-    border-radius: 6px;
-    border: 1px solid #fff;
-    color: #fff;
-    background: inherit;
-    cursor: pointer;
-    font-size: 16px;
-    transition: all ease 0.3s;
-
-    &:hover {
-      background-color: #fff;
-      color: #0d0f11;
-    }
-  }
 }
 </style>
